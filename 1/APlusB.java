11cb5700ae13da5bf727dbe0362bad79c1c3dd09
ac12341234 <apple72601@gmail.com>
2026-05-18 20:12:04 +0800

    problem1 javadoc

--- 1/APlusB.java
+++ 1/APlusB.java
@@ -1,4 +1,26 @@
+/**
+ * 一个简单的加法工具类。
+ * <p>
+ * 该类提供了一个静态方法，用于计算两个整数的和。
+ * </p>
+ *
+ * @author 你的名字
+ * @version 1.0
+ * @since 1.0
+ */
 public class APlusB {
+
+    /**
+     * 计算两个整数的和。
+     * <p>
+     * 该方法接收两个整数参数，返回它们的算术和。
+     * 示例：{@code plus(3, 5)} 返回 {@code 8}。
+     * </p>
+     *
+     * @param a 第一个加数，整数类型
+     * @param b 第二个加数，整数类型
+     * @return 两个整数的和，即 a + b
+     */
     public static int plus(int a, int b) {
         return a + b;
     }
